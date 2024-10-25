9954e43e05abf36ece6a3846b01f705eb009d256
深川　貴司 <fukagawatakashiee893@gmail.com>
2024-10-25 15:51:03 +0900

    icon

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -16,15 +16,35 @@
 			link: '/',
 		}
 	];
+
+	let icons = [
+		{
+			icon: '/read.svg',
+			link: '/curriculum',
+		},
+		{
+			icon: '/plan.svg',
+			link: '/plan',
+		},
+		{
+			icon: '/mail.svg',
+			link: '/',
+		}
+	];
 </script>
 
 <template lang='pug'>
 	header.fixed.t0.l0.z100.w-full.bg-black
 		div.f.fm.fbw.h80.px20.text-white.fs16.s-fs14
-			a(href='/') プログラミング
-			div
+			a(href='/')
+				img.h40.block.object-fit-contain(src='/pc_logo.png')
+			div.s-hide
 				+each('headerItems as item')
 					a.mr24.mr0-last(href='{item.link}') {item.label}
+			div.f.fm.s-show
+				+each('icons as icon')
+					a.mr24.mr0-last(href='{icon.link}')
+						img.block.object-fit-contain.h30(src='{icon.icon}')
 </template>
 
 
