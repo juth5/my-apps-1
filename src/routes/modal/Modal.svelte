226b6d9d9dc0d4230de2f3a31f53a27dcdddd4c3
深川　貴司 <fukagawatakashiee893@gmail.com>
2024-11-16 12:01:36 +0900

    modal

---
+++ src/routes/modal/Modal.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let show = false;
+  export let onClose = () => {};
+</script>
+<template lang='pug'>
+  +if('show')
+    div.modal-backdrop(on:click!='{onClose}')
+      div.modal-content(on:click|stopPropagation)
+        slot
+        button(on:click!='{onClose}') 閉じる
+</template>
+
+<style>
+  .modal-backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+  .modal-content {
+    background-color: #fff;
+    margin: 10% auto;
+    padding: 20px;
+    width: 80%;
+    max-width: 500px;
+    border-radius: 8px;
+  }
+</style>
