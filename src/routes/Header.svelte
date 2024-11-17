afcac9914bc50a0ad7ffd82d12a9d40010a7b305
深川　貴司 <fukagawatakashiee893@gmail.com>
2024-11-17 20:31:00 +0900

    target=blank

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -38,15 +38,15 @@
 <template lang='pug'>
 	header.fixed.t0.l0.z100.w-full.bg-black
 		div.f.fm.fbw.h80.px20.text-white.fs16.s-h50.s-fs14
-			a.s-hide(href='/') プログラミング
+			a.s-hide(href='/') 福岡でプログラミング
 			a.s-show(href='/')
 				img.h30.block.object-fit-contain(src='/home.svg')
 			div.s-hide
 				+each('headerItems as item')
-					a.mr24.mr0-last(href='{item.link}', target='blank') {item.label}
+					a.mr24.mr0-last(href='{item.link}') {item.label}
 			div.f.fm.s-show
 				+each('icons as icon')
-					a.mr24.mr0-last(href='{icon.link}', target='blank')
+					a.mr24.mr0-last(href='{icon.link}')
 						img.block.object-fit-contain.h30(src='{icon.icon}')
 </template>
 
